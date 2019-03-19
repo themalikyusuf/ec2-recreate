1f4b217fe352effad1504aed6bf76f34e4d33147
abdulmalik yusuf <themalikyusuf@gmail.com>
2019-03-19 18:11:25 +0100

    chore: update userdata

--- roles/common/templates/userdata.sh
+++ roles/common/templates/userdata.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -o errexit
 set -o xtrace
-apt-get -y update
+apt-get -y update && apt-get upgrade
 apt-get -y install apt-transport-https openjdk-8-jre-headless uuid-runtime pwgen
 
 # Install MongoDB
@@ -16,7 +16,7 @@ systemctl restart mongod.service
 # Install ElasticSearch
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
 echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" | tee -a /etc/apt/sources.list.d/elastic-5.x.list
-apt-get update && apt-get install elasticsearch
+apt-get update && apt-get install -y elasticsearch
 sed -i -e 's/#cluster.name: my-application/cluster.name: graylog/g' /etc/elasticsearch/elasticsearch.yml
 systemctl daemon-reload
 systemctl enable elasticsearch.service
@@ -25,7 +25,7 @@ systemctl restart elasticsearch.service
 # Install Graylog
 wget https://packages.graylog2.org/repo/packages/graylog-2.4-repository_latest.deb
 dpkg -i graylog-2.4-repository_latest.deb
-apt-get update && apt-get install graylog-server
+apt-get update && apt-get install -y graylog-server
 sed -i -e "s/password_secret =.*/password_secret = $(pwgen -s 128 1)/" /etc/graylog/server/server.conf
  sed -i -e "s/root_password_sha2 =.*/root_password_sha2 = $(echo -n 'password' | shasum -a 256 | cut -d' ' -f1)/" /etc/graylog/server/server.conf
 sed -i -e "s|rest_listen_uri = http://127.0.0.1:9000/api/|rest_listen_uri = http://$(ip route get 8.8.8.8 | awk '{print $NF; exit}'):9000/api/|g" /etc/graylog/server/server.conf
